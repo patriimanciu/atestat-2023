728fb4368d4efdfb12ffbfb43c26cac55f1615ce
Patri Manciu <patri.manciu@gmail.com>
2023-04-26 21:15:21 +0300

    add 4

---
+++ matrice/21/21.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream cin("atestat.in");
+ofstream cout("atestat.out");
+
+int main()
+{
+    int n, a[21][21], k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++){
+            
+        }
+
+    return 0;
+}
