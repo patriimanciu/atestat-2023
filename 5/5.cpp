df5d83e86eeeb307672662ebea8cb586d4ba4974
Patri Manciu <patri.manciu@gmail.com>
2023-05-03 22:53:45 +0300

    spring cleaning

---
+++ 5/5.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+
+using namespace std;
+
+ifstream cin("date.in");
+ofstream cout("date.out");
+
+int CifrePare(int n){
+    int p = 1, nr = 0;
+    while (n){
+        if (n % 2 == 0){
+            nr = nr + (n % 10) * p;
+            p *= 10;
+        }
+        n /= 10;
+    }
+    return nr;
+}
+
+int CifreCresc(int n){
+    while (n > 9){
+        if (n % 10 < (n / 10) % 10)
+            return 0;
+        n /= 10;
+    }
+    return 1;
+}
+
+int main(){
+    int n, sum = 0;
+    while (cin >> n){
+        if (CifreCresc(CifrePare(n)))
+            sum += CifrePare(n);
+    }
+    cout << sum;
+    return 0;
+}
