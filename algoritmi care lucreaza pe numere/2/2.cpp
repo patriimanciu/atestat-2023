2997a96d1c01b7c6148dd83131b67b070a77af71
Patri Manciu <patri.manciu@gmail.com>
2023-05-05 00:05:02 +0300

    add 2

--- algoritmi care lucreaza pe numere/2/2.cpp
+++ algoritmi care lucreaza pe numere/2/2.cpp
@@ -1,8 +1,44 @@
-#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
-{
+ifstream cin("date.in");
+ofstream cout("date.out");
+
+int FactorPutereMax(int n){
+    int base, power = 0, p = 0, i = 2;
+    while(n > 1){
+        if (n % i == 0){
+            n /= i;
+            p++;
+            if (p >= power){
+                power = p;
+                base = i;
+            }
+        }
+        else{
+            if (p >= power){
+                power = p;
+                base = i;
+                p = 0;
+            }
+            if (i == 2)
+                i = 3;
+            else
+                i += 2;
+        }
+    }
+    return base;
+}
+
+int main(){
+    int n, maxx = 0, m;
+    while (cin >> n){
+        m = FactorPutereMax(n);
+        if (m > maxx)
+            maxx = m;
+        cout << m << " ";
+    }
+    cout << '\n' << maxx;
     return 0;
 }
