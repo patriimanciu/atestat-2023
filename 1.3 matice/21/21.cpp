0305e12cf958753b531ad06cea6ddaf9fd28ef17
Patri Manciu <patri.manciu@gmail.com>
2023-05-08 21:38:10 +0300

    add 21

--- 1.3 matice/21/21.cpp
+++ 1.3 matice/21/21.cpp
@@ -8,11 +8,23 @@ ofstream cout("atestat.out");
 int main()
 {
     int n, a[21][21], k;
+    bool found;
     cin >> n >> k;
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++){
-            
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+    for (int i = 0; i < n; i++){
+        if (i == k)
+            continue;
+        found = false;
+        for (int j = 0; j < n && !found; j++){
+            for (int m = 0; m < n && !found; m++) 
+                if (a[i][j] == a[k][m]){
+                    cout << i + 1 << " ";
+                    found = true;
+                }
         }
+    }
 
     return 0;
 }
