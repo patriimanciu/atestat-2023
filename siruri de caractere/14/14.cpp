1d1250c469addf542e8618dd63791336ab5ce2c2
Patri Manciu <patri.manciu@gmail.com>
2023-05-08 21:08:45 +0300

    add 14

---
+++ siruri de caractere/14/14.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+ifstream cin("cuvant.in");
+ofstream cout("numar.out");
+
+int main(){
+    char s[31], voc[] = "aeiou";
+    int nr = 0; 
+    cin >> s;
+    if (strlen(s) > 17)
+        cout << "Imposibil";
+    else{
+        for (int i = 0; i < strlen(s); i++)
+            if (!strchr(voc, s[i]))
+                if(i + 1 < 10)
+                    nr = nr * 10 + (i + 1); 
+                else
+                    nr = nr * 10 + (i + 1 - 10);
+        if (nr == 0)
+            cout << "Imposibil";
+        else
+            cout << nr;
+    }
+    return 0;
+}
